b56ab4c9a404a968e33d8041cbcaf2f157a88d97
Gappleg <kj109888@gmail.com>
2020-12-03 14:19:23 +0900

    video upload page

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React , { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import Dropzone from 'react-dropzone';
 
 const { Title } = Typography;
 const { TextArea } = Input;
 
+const PrivateOptions = [
+    {value: 0, label: "Private"},
+    {value: 1, label: "Public"}
+]
+
+const CategoryOptions = [
+    {value: 0, label: "Film & Animation"},
+    {value: 1, label: "Autos & Vehicles"},
+    {value: 3, label: "Music"},
+    {value: 4, label: "Pets & Animals"}
+]
+
 function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation") // default state
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value) // e.currentTarget.value 입력창에 치는 값이 event로 발생, 가져와서 state를 변경
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value)
+    }
+    
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
+
             <div style={{ textAlign:'center', marginBottom:'2rem' }}>
                 <Title level={2}>Upload Video</Title>
             </div>
@@ -19,10 +54,10 @@ function VideoUploadPage() {
                     onDrop
                     multiple
                     maxSize>
-                    {({ getRootProps, getInputProps}) => (
+                    {({ getRootProps, getInputProps }) => (
                         <div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
                     alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
-                        {/* <input {...getRootProps()} /> */}
+                        <input {...getInputProps()} />
                         <Icon type="plus" style={{ fontSize:'3rem'}} />
                     </div>
                     )}
@@ -32,34 +67,48 @@ function VideoUploadPage() {
                         <img src alt />
                     </div>
                 </div>
-        <br />
-        <br />
-        <label>Title</label>
-        <Input 
-            onChange
-            value
-        />
-        <br />
-        <br />
-        <label>Description</label>
-        <TextArea
-            onChange
-            value
-        />
-        <br />
-        <br />
+                <br />
+                <br />
 
-        <select onChange>
-            <option key value></option>
-        </select>
+                {/* Title */}
+                <label>Title</label>
+                <Input 
+                    onChange={onTitleChange}
+                    value={VideoTitle}
+                />
+                <br />
+                <br />
 
-        <select onChange>
-            <option key value></option>
-        </select>
+                {/* Description */}
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={Description}
+                />
+                <br />
+                <br />
 
-        <Button type="primary" size="large" onClick>
-            Submit
-        </Button>
+                {/* Private Change */}
+                <select onChange={onPrivateChange}>
+                    {PrivateOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
+                </select>
+                <br />
+                <br />
+
+                {/* Category Change */}
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
+                </select>
+                <br />
+                <br />
+
+                <Button type="primary" size="large" onClick>
+                    Upload
+                </Button>
             </Form>
         </div>
     )
